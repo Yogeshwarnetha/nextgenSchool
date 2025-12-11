4c216b9e37710dd48adf550c5cc49497ff3f42f5
Yogeshwarnetha <gadde.yogeshwar@gmail.com>
2025-12-12 02:55:39 +0530

    Added

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const nextConfig: NextConfig = {
     domains: ['sgpshtest.blob.core.windows.net', 'propertysourcehub.co.uk', 'www.gbdarchitects.com', '5.imimg.com', 'media.istockphoto.com', 'example.com', 'images.unsplash.com', 'pub-56ba1c6c262346a6bcbe2ce75c0c40c5.r2.dev', 'pub-5508d64e14364eca9f48ef0efa18bda5.r2.dev', 'pub-837447cab048469baef2e30fbd0a9877.r2.dev'],
 
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
